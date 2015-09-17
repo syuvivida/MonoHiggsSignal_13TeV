9f0323f7353e87ee3d4c2e7aa1e024c296f598c1
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-09-17 16:29:31 +0200

    update output script

--- Zp2HDM/scripts/outputgoogle.bash
+++ Zp2HDM/scripts/outputgoogle.bash
@@ -10,24 +10,19 @@ then
     exit 1
 fi
 
-rm -rf file.txt
-rm -rf cvmfs.txt
-rm -rf url.txt
 
 type=$1
 version=$2
 process=${type%%_MZp*}
 echo $process
-if [[ ! -e $process ]]; then 
-    mkdir $process
-fi
+
 location="/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.0/monoHiggs/Zp2HDM/${process}/${version}/${type}_tarball.tar.xz"
 echo $location >> cvmfs.txt
 
 echo "https://github.com/cms-sw/genproductions/tree/4e6dda7ecc882f106135d5a33c602f53bc4843a9/bin/MadGraph5_aMCatNLO/cards/production/13TeV/monoHiggs/Zp2HDM/"$process"/"$type >> url.txt
 
 
-filename=${type}_13TeV-madgraph_cff.py
+filename=${type}_13TeV-madgraph
 echo $filename >> file.txt
 
 
